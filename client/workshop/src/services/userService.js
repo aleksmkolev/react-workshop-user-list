7c338540c8104dfb2e40fa10d45d73aad71a2224
Alexander Kolev <165174435+aleksmkolev@users.noreply.github.com>
2025-03-21 23:32:43 +0000

    fix time on add

--- client/workshop/src/services/userService.js
+++ client/workshop/src/services/userService.js
@@ -8,12 +8,16 @@ export default {
         return users
     },
     async create(userData){
+        const { country, city, street, streetNumber, ...postData } = userData
+         postData.address = `${street} ${streetNumber}, ${city}, ${country}`
+         postData.createdAt = new Date().toISOString()
+         postData.updatedAt = new Date().toISOString()
         const response = await fetch(baseUrl, {
             method: 'POST', 
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(userData)
+            body: JSON.stringify(postData)
         })
         const result = await response.json()
         return result
